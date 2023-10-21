2a630d8e3be0ecf154a77aefc1c95c6d5a6560ee
AndreiReznikov <andrezniksk@yandex.ru>
2023-10-21 19:16:28 +0800

    added all animations of the main page

--- src/pages/landing-page/LandingPage.js
+++ src/pages/landing-page/LandingPage.js
@@ -4,13 +4,34 @@ class LandingPage {
   init() {
     this._findElements();
     this._setVideo();
+    this._startInitialAnimation();
     this._addEventListeners();
   }
 
   _findElements() {
     this.$window = $(window);
     this.$landingPageBackground = $('.landing-page__background');
+    this.$landingPageTitle = $('.landing-page__title');
+    this.$landingPageScrim = $('.landing-page__scrim');
     this.$video = $('video');
+  }
+
+  _startInitialAnimation() {
+    setTimeout(() => this._setBackgroundPosition('center'));
+    setTimeout(() => {
+      this.$landingPageBackground.css({
+        color: 'rgba(0, 0, 0, 1)',
+        'mix-blend-mode': 'lighten',
+      });
+      this.$landingPageScrim.css({
+        color: 'rgba(0, 0, 0, 1)',
+      });
+      this.$landingPageTitle.css({
+        color: '#fff',
+      });
+      this.$landingPageBackground.text('Марс');
+      this.$landingPageScrim.text('Марс');
+    }, 1200);
   }
 
   _setBackgroundPosition(position) {
@@ -18,6 +39,8 @@ class LandingPage {
   }
 
   _shiftBackground(event) {
+    this.$landingPageBackground.css({ transition: '0.6s' });
+
     this.$windowWidth = this.$window.width();
     this.clientX = event.pageX;
     let position = 'center';
