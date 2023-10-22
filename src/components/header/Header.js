23629e67c402fc3b19c35c2e4aa9b94ae799954e
AndreiReznikov <andrezniksk@yandex.ru>
2023-10-22 19:38:04 +0800

    the central elements are aligned

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,21 +7,25 @@ class Header {
   _findElements() {
     this.$burger = $('.header__burger');
     this.$header = $('.header');
+    this.$headerButton = $('.header__button-container button');
+    this.$burgerContainer = this.$header.find('.header__burger-container');
+    this.$burger = this.$header.find('.header__burger');
+    this.$mobileMenu = this.$header.find('.header__mobile-menu');
+    this.$html = $('html');
   }
 
   _toggleMobileMenu() {
-    const $burgerContainer = this.$header.find('.header__burger-container');
-    const $burger = this.$header.find('.header__burger');
-    const $mobileMenu = this.$header.find('.header__mobile-menu');
-    const $html = $('html');
-    const htmlClass = $html.attr('class');
+    const htmlClass = this.$html.attr('class');
     const handleBurgerContainerToggleMobileMenu = () => {
-      $mobileMenu.toggleClass('header__mobile-menu_visible');
-      $burger.toggleClass('header__burger_transformed');
-      $html.toggleClass(`${htmlClass}_non-scrollable`);
+      const isButtonDisables = this.$headerButton.prop('disabled');
+
+      this.$mobileMenu.toggleClass('header__mobile-menu_visible');
+      this.$burger.toggleClass('header__burger_transformed');
+      this.$html.toggleClass(`${htmlClass}_non-scrollable`);
+      this.$headerButton.attr('disabled', !isButtonDisables);
     };
 
-    $burgerContainer.on('click', handleBurgerContainerToggleMobileMenu);
+    this.$burgerContainer.on('click', handleBurgerContainerToggleMobileMenu);
   }
 }
 
